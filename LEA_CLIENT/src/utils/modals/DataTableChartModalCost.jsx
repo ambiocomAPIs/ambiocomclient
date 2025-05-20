e55ef59daeb0eeb32984728d574a5688b5889cff
ambiocomAPIs <ambiocomambientedepruebas@gmail.com>
2025-05-20 09:12:01 -0500

    ajuste prueba

--- LEA_CLIENT/src/utils/modals/DataTableChartModalCost.jsx
+++ LEA_CLIENT/src/utils/modals/DataTableChartModalCost.jsx
@@ -186,7 +186,7 @@ const DataTableChartModalCost = ({ modalIsOpen, closeModal }) => {
           bottom: 'auto',
           transform: 'translate(-50%, -50%)',
           width: '80vw',
-          height: '85vh',
+          height: '80vh',
           maxHeight: '90vh',
           padding: '30px',
           borderRadius: '12px',
