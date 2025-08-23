66000cea2127b175a2ca6fe34cc9cad96bbbdc47
ambiocomAPIs <ambiocomambientedepruebas@gmail.com>
2025-08-23 00:37:07 -0500

    ajuste context

--- LEA_CLIENT/src/utils/Context/TanquesContext.jsx
+++ LEA_CLIENT/src/utils/Context/TanquesContext.jsx
@@ -11,8 +11,7 @@ export const TanquesProvider = ({ children }) => {
 
   const fetchTanques = async () => {
     try {
-      const res = await axios.get("
-https://ambiocomserver.onrender.com/api/tanques");
+      const res = await axios.get("https://ambiocomserver.onrender.com/api/tanques");
       setTanques(res.data);
     } catch (error) {
       console.error("❌ Error al cargar tanques:", error);
